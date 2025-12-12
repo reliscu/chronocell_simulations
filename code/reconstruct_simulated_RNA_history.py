9a160b4c28c4e45a32ff81ee2ab392dfc001817b
reliscu <rebeliscu@gmail.com>
2025-12-12 10:29:04 -0800

    code for calculating all cells at once

--- code/reconstruct_simulated_RNA_history.py
+++ code/reconstruct_simulated_RNA_history.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
 import scipy.linalg as la
+from scipy.stats import poisson
 from functools import lru_cache
 from scipy.sparse.linalg import expm_multiply, eigs
 from scipy.sparse import coo_matrix, csr_matrix, diags
@@ -57,18 +58,24 @@ def create_transition_matrix(reactions, U_max, S_max):
     
     return A
 
-def stationary_from_transition_matrix(A): 
-    w, v = np.linalg.eig(A.T)
-    idx = np.argmin(np.abs(w)) # Eigenvector corresponding to eigenvalue closest to 0
-    pi = np.real(v[:, idx])
-    if pi.sum() < 0:
-        pi = -pi
-    pi[pi < 0] = 0.0
-    pi /= pi.sum()
+def stationary_from_params(alpha, beta, gamma, states):
+    mean_u = alpha / beta
+    mean_s = alpha / gamma
+
+    # unpack states into arrays
+    u_vals = np.array([u for (u, s) in states], dtype=int)
+    s_vals = np.array([s for (u, s) in states], dtype=int)
+
+    # Poisson pmf for each state coordinate
+    p_u = poisson.pmf(u_vals, mean_u)
+    p_s = poisson.pmf(s_vals, mean_s)
+
+    pi = p_u * p_s  # independent U and S
+    pi /= pi.sum()  # renormalize on truncated grid
+
     return pi
 
 def reverse_generator(A, mu):
-    
     ## Some refs for getting reverse time Markov generator:
     ## https://arxiv.org/abs/2502.19183 (p. 3)
     ## https://www.randomservices.org/random/markov/TimeReversal2.html
@@ -86,7 +93,7 @@ A_gene = None
 X_fwd_gene = None
 
 @lru_cache(maxsize=None)
-def get_expm_per_gene(state_idx, dt):
+def get_expm(state_idx, dt):
     """
     Cached expm(A_gene[state_idx] * dt).
     """
@@ -94,7 +101,7 @@ def get_expm_per_gene(state_idx, dt):
     return la.expm(A_k * dt)
 
 @lru_cache(maxsize=None)
-def get_expm_rev_per_gene(state_idx, k, dt):
+def get_expm_rev(state_idx, k, dt):
     """
     Cached expm(A_rev(state_idx, k) * dt) for BACKWARD direction.
     """
@@ -129,21 +136,34 @@ def forward_distribution(A, Y, pi, states, t, tau, state_grid):
             dt = t_next - t_curr
             # A_k = A[state_curr]
             # x_next = x_curr @ sp.linalg.expm(A_k * dt) 
-            M = get_expm_per_gene(state_curr, dt)
+            M = get_expm(state_curr, dt)
             x_next = x_curr @ M 
-            
+        
         else:   
             # State switch happens in current interval
             t_s = tau[state_next]
 
-            # Split backward march into 2 steps
+            # Split forward march into 2 steps
             dt1 = t_s - t_curr # left interval: [t_k, state_switch_time)
-            A_k1 = A[state_curr]
-            x_mid = x_curr @ sp.linalg.expm(A_k1 * dt1)
+            M1 = get_expm(state_curr,  dt1)
+            x_mid = x_curr @ M1  
       
             dt2 = t_next - t_s # right interval: [state_switch_time, t_{k+1})
-            A_k2 = A[state_next]
-            x_next = x_mid @ sp.linalg.expm(A_k2 * dt2) 
+            M2 = get_expm(state_next, dt2)
+            x_next = x_mid @ M2
+            
+        # else:   
+        #     # State switch happens in current interval
+        #     t_s = tau[state_next]
+
+        #     # Split backward march into 2 steps
+        #     dt1 = t_s - t_curr # left interval: [t_k, state_switch_time)
+        #     A_k1 = A[state_curr]
+        #     x_mid = x_curr @ sp.linalg.expm(A_k1 * dt1)
+      
+        #     dt2 = t_next - t_s # right interval: [state_switch_time, t_{k+1})
+        #     A_k2 = A[state_next]
+        #     x_next = x_mid @ sp.linalg.expm(A_k2 * dt2) 
         
         X_fwd[:, k+1] = x_next
         
@@ -157,25 +177,19 @@ def forward_distribution(A, Y, pi, states, t, tau, state_grid):
     X_fwd_gene = X_fwd
     return X_fwd
 
-def backward_distribution(Y, A, X_bw, X_fwd, states, t, tau, state_grid):
+def backward_distribution(Y, X_bw, states, t, tau, state_grid):
     global A_gene, X_fwd_gene
-    A_gene = A
 
     # For each time step, calculate previous state using current state
 
     for k in reversed(range(1, len(t))):
         t_prev, t_curr = t[k-1], t[k]
         state_prev, state_curr = state_grid[k-1], state_grid[k]
-
         x_curr = X_bw[:, k]
-        mu_k = X_fwd[:, k]
             
         if state_prev == state_curr:
             dt = t_curr - t_prev
-            # A_k = A[state_curr]
-            # A_k_rev = reverse_generator(A_k, mu_k) 
-            # x_prev = expm_multiply(A_k_rev.T * dt, x_curr) # x_curr @ sp.linalg.expm(A_k_rev * dt)
-            M_rev = get_expm_rev_per_gene(state_curr, k, dt)
+            M_rev = get_expm_rev(state_curr, k, dt)
             x_prev = x_curr @ M_rev
         
         else:
@@ -184,15 +198,13 @@ def backward_distribution(Y, A, X_bw, X_fwd, states, t, tau, state_grid):
 
             # Split backward march into 2 steps
             dt2 = t_curr - t_s # right interval: (state_switch_time, t_k]
-            A_k2 = A[state_curr]
-            A_k2_rev = reverse_generator(A_k2, mu_k)
-            x_mid = expm_multiply(A_k2_rev.T * dt2, x_curr) # x_curr @ sp.linalg.expm(A_k2_rev * dt2) 
+            M2_rev = get_expm_rev(state_curr, k, dt2)
+            x_mid = x_curr @ M2_rev 
             
             dt1 = t_s - t_prev # left interval: (t_{k-1}, state_switch_time]
-            A_k1 = A[state_prev]
-            A_k1_rev = reverse_generator(A_k1, mu_k) 
-            x_prev = expm_multiply(A_k1_rev.T * dt1, x_mid) # x_mid @ sp.linalg.expm(A_k1_rev * dt1) 
-    
+            M1_rev = get_expm_rev(state_prev, k, dt1)
+            x_prev = x_mid @ M1_rev  
+
         X_bw[:, k-1] = x_prev
         
         ###########################################################################
@@ -205,6 +217,60 @@ def backward_distribution(Y, A, X_bw, X_fwd, states, t, tau, state_grid):
         print("-------") 
         
     return X_bw
+
+def backward_distribution_cells(Y, gene_idx, states, index_for, t, tau, state_grid):
+    # Y: U and S count matrices
+
+    global A_gene, X_fwd_gene
+    
+    n_cells = Y.shape[0]
+
+    # Initialize backwards trajectory with observed counts for each cell
+    U_curr, S_curr = np.round(Y[:, gene_idx, 0]), np.round(Y[:, gene_idx, 1])
+    X_curr = np.zeros(shape=(len(states), n_cells), dtype="float")
+    for cell_idx in range(n_cells):
+        X_curr[index_for[(U_curr[cell_idx], S_curr[cell_idx])], cell_idx] = 1.0 
+        
+    X_bw = np.zeros(shape=(len(states), len(t), Y.shape[0])) 
+
+    # Start backwards trajectory at cell's inferred position in time
+    t_obs = np.asarray([np.searchsorted(t, i) for i in t])
+
+    for k in reversed(range(1, len(t))):
+        t_prev, t_curr = t[k-1], t[k]
+        state_prev, state_curr = state_grid[k-1], state_grid[k]
+
+        # Only calculate backwards trajectory for cells at t = t[k] or later
+        mask = t_obs >= k
+        X_bw[:, k, mask] = X_curr[:, mask]
+
+        if state_prev == state_curr:
+            dt = t_curr - t_prev
+            M_rev = get_expm_rev(state_curr, k, dt)
+            X_prev = (X_curr[:, mask].T @ M_rev).T
+            
+        else:
+            # State switch happens in current interval             
+            t_s = tau[state_curr]
+
+            # Split backward march into 2 steps
+            dt2 = t_curr - t_s # right interval: (state_switch_time, t_k]
+            M2_rev = get_expm_rev(state_curr, k, dt2)
+            X_mid = (X_curr[:, mask].T @ M2_rev)
+            
+            dt1 = t_s - t_prev # left interval: (t_{k-1}, state_switch_time]
+            M1_rev = get_expm_rev(state_prev, k, dt1)
+            X_prev = (X_mid @ M1_rev).T
+
+        X_curr[:, mask] = X_prev
+        X_bw[:, k-1, mask] = X_prev
+
+    # Cell's at t=0 only have observed data
+    mask = t_obs == 0   
+    X_bw[:, 0, mask] = X_curr[:, mask]
+        
+    return X_bw
+
 
 def marginal_distribution(X, U_max, S_max, t, states):
     X_u = np.zeros(shape=(U_max + 1, len(t)))
@@ -221,3 +287,17 @@ def marginal_distribution(X, U_max, S_max, t, states):
             X_s[i, k] = np.sum(X[u_state_indices, k])
     
     return X_u, X_s
+
+
+###############
+
+
+# def stationary_from_transition_matrix(A): 
+#     w, v = np.linalg.eig(A.T)
+#     idx = np.argmin(np.abs(w)) # Eigenvector corresponding to eigenvalue closest to 0
+#     pi = np.real(v[:, idx])
+#     if pi.sum() < 0:
+#         pi = -pi
+#     pi[pi < 0] = 0.0
+#     pi /= pi.sum()
+#     return pi
